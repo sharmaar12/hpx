664fcaeadb91f78fd4e55562b889754bbf75d823
Anuj Sharma <sharma.anuj1991@gmail.com>
2014-07-05 18:11:27 +0530

    Added the async API of hpx::for_each() in algorithm.hpp

--- hpx/components/vector/algorithm.hpp
+++ hpx/components/vector/algorithm.hpp
@@ -11,44 +11,68 @@
 #define ALGORITHM_HPP
 
 #include <hpx/include/util.hpp>
+#include <hpx/include/lcos.hpp>
 #include <hpx/components/vector/chunk_vector_component.hpp>
 
 namespace hpx
 {
  //   template<class input_iterator, class fun>
-        void for_each(hpx::segmented_vector_iterator first, segmented_vector_iterator last, hpx::util::function<void(VALUE_TYPE&)> fn_)
+        void for_each(hpx::segmented_vector_iterator first, segmented_vector_iterator last, hpx::util::function<void(VALUE_TYPE&)> fn)
         {
             auto sfirst_ = segmented_vector_iterator::segment(first);
             auto slast_ = segmented_vector_iterator::segment(last);
 
+            std::vector<hpx::lcos::future<void>> for_each_lazy_sync;
+
             if(sfirst_ == slast_)
-                hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::local(first).first).get(), //gives gid
+            {
+                for_each_lazy_sync.push_back(
+                        hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::local(first).first).get(), //gives gid
                                              segmented_vector_iterator::local(first).second, // gives first index
                                              segmented_vector_iterator::local(last).second, //gives last index
-                                             fn_); //gives the functor
+                                             fn) //gives the functor
+                                             );
+            }
             else
             {
-                hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::local(first).first).get(), //gives gid
+                for_each_lazy_sync.push_back(
+                        hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::local(first).first).get(), //gives gid
                                              segmented_vector_iterator::local(first).second, // gives first index
                                              segmented_vector_iterator::end(sfirst_).second, //gives last index
-                                             fn_); //gives the functor
+                                             fn) //gives the functor
+                                             );
                 ++sfirst_;
                 while(sfirst_ != slast_)
                 {
-                    hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::begin(sfirst_).first).get(), //gives gid
-                                             segmented_vector_iterator::begin(sfirst_).second, // gives first index
-                                             segmented_vector_iterator::end(sfirst_).second, //gives last index
-                                             fn_); //gives the functor
+                    for_each_lazy_sync.push_back(
+                                hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::begin(sfirst_).first).get(), //gives gid
+                                                 segmented_vector_iterator::begin(sfirst_).second, // gives first index
+                                                 segmented_vector_iterator::end(sfirst_).second, //gives last index
+                                                 fn) //gives the functor
+                                                  );
                     ++sfirst_;
                 }
-
-                hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::begin(slast_).first).get(), //gives gid
+                for_each_lazy_sync.push_back(
+                            hpx::stubs::chunk_vector::chunk_for_each_async( (segmented_vector_iterator::begin(slast_).first).get(), //gives gid
                                              segmented_vector_iterator::begin(slast_).second, // gives first index
                                              segmented_vector_iterator::local(last).second, //gives last index
-                                             fn_); //gives the functor
+                                             fn) //gives the functor
+                                             );
             }//end of else
 
+            hpx::wait_all(for_each_lazy_sync);
         }//end of for_each
+
+        hpx::lcos::future<void> for_each_async(hpx::segmented_vector_iterator first, segmented_vector_iterator last, hpx::util::function<void(VALUE_TYPE&)> fn)
+        {
+            return hpx::async(launch::async,
+                              hpx::util::bind((&hpx::for_each),
+                                               first,
+                                               last,
+                                               fn
+                                              )
+                              );
+        }//end of for_each_async
 }//end of hpx namespace
 
 #endif // ALGORITHM_HPP
