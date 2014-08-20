f58d74f118f134958bcc8e869e7d1d168f5a1d18
Anuj Sharma <sharma.anuj1991@gmail.com>
2014-08-20 12:31:34 +0530

    Corrected typos in documentation from the chunk_vector_component.cpp

--- src/components/vector/chunk_vector_component.cpp
+++ src/components/vector/chunk_vector_component.cpp
@@ -19,93 +19,117 @@ typedef hpx::components::managed_component<hpx::server::chunk_vector>
 
 HPX_REGISTER_MINIMAL_COMPONENT_FACTORY(chunk_vector_type, chunk_vector);
 
-
 //Capacity related action registration
-/** @brief Macro to define the boilerplate code for size component action.*/
+/** @brief Macro to define the boilerplate code for \a size component action.*/
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::size_action,
     chunk_vector_size_action);
-/** @brief Macro to define the boilerplate code for max_size component action.*/
+/** @brief Macro to define the boilerplate code for \a max_size component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::max_size_action,
     chunk_vector_max_size_action);
-/** @brief Macro to define the boilerplate code for resize component action.*/
+/** @brief Macro to define the boilerplate code for \a resize component action.
+*/
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::resize_action,
     chunk_vector_resize_action);
-/** @brief Macro to define the boilerplate code for capacity component action.*/
+/** @brief Macro to define the boilerplate code for \a capacity component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::capacity_action,
     chunk_vector_capacity_action);
-/** @brief Macro to define the boilerplate code for empty component action.*/
+/** @brief Macro to define the boilerplate code for \a empty component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::empty_action,
     chunk_vector_empty_action);
-/** @brief Macro to define the boilerplate code for reserve component action.*/
+/** @brief Macro to define the boilerplate code for \a reserve component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::reserve_action,
     chunk_vector_reserve_action);
 
 //Element access component action registration
-/** @brief Macro to define the boilerplate code for get_value_noexpt component
- *          action.
+/** @brief Macro to define the boilerplate code for \a get_value_noexpt
+ *          component action.
  */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::get_value_noexpt_action,
     chunk_vector_get_value_noexpt_action);
-/** @brief Macro to define the boilerplate code for get_value component action.*/
+/** @brief Macro to define the boilerplate code for \a get_value component
+ *          action.\
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::get_value_action,
     chunk_vector_get_value_action);
-/** @brief Macro to define the boilerplate code for front component action.*/
+/** @brief Macro to define the boilerplate code for \a front component action.*/
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::front_action,
     chunk_vector_front_action);
-/** @brief Macro to define the boilerplate code for back component action.*/
+/** @brief Macro to define the boilerplate code for \a back component action.*/
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::back_action,
     chunk_vector_back_action);
 
 ////Modifiers component action registration
-/** @brief Macro to define the boilerplate code for assign component action.*/
+/** @brief Macro to define the boilerplate code for \a assign component action.
+*/
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::assign_action,
     chunk_vector_assign_action);
-/** @brief Macro to define the boilerplate code for push_back component action.*/
+/** @brief Macro to define the boilerplate code for \a push_back component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::push_back_action,
     chunk_vector_push_back_action);
-/** @brief Macro to define the boilerplate code for push_back_rval component
+/** @brief Macro to define the boilerplate code for \a push_back_rval component
 *           action.
 */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::push_back_rval_action,
     chunk_vector_push_back_rval_action);
-/** @brief Macro to define the boilerplate code for pop_back component action.*/
+/** @brief Macro to define the boilerplate code for \a pop_back component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::pop_back_action,
     chunk_vector_pop_back_action);
-/** @brief Macro to define the boilerplate code for set_value component action.*/
+/** @brief Macro to define the boilerplate code for \a set_value component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::set_value_action,
     chunk_vector_set_value_action);
-/** @brief Macro to define the boilerplate code for set_value_rval component
+/** @brief Macro to define the boilerplate code for \a set_value_rval component
  *          action.
  */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::set_value_rval_action,
     chunk_vector_set_value_rval_action);
 
+/** @brief Macro to define the boilerplate code for \a clear component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::clear_action,
     chunk_vector_clear_action);
 
 //Algorithm API's component action registration
-/** @brief Macro to define the boilerplate code for chunk_for_each component action.*/
+/** @brief Macro to define the boilerplate code for \a chunk_for_each component
+ *          action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::chunk_for_each_action,
     chunk_vector_chunk_for_each_action);
-/** @brief Macro to define the boilerplate code for chunk_for_each_const component action.*/
+/** @brief Macro to define the boilerplate code for \a chunk_for_each_const
+ *          component action.
+ */
 HPX_REGISTER_ACTION(
     hpx::server::chunk_vector::chunk_for_each_const_action,
     chunk_vector_chunk_for_each_const_action);
